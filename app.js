d0ca96ee6b921c26b18c704ad77f62dd17a11306
gc200295036 <nickbrazda95@hotmail.com>
2021-04-06 19:47:07 -0400

    project upload

---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var app = express();
+
+// require mongoose
+const mongoose = require('mongoose')
+// dbConfiguration file
+const config = require('./dbConfiguration/database')
+//dbConfiguration/database file now stored as 'config' variable.
+// config.database so that my connection string isn't hard coded in app.js
+mongoose.connect(config.database,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then((res) => {
+  console.log('CONNECTION SUCCESS! using fishHuntStore database @ MongoDB')
+  }).catch(() => {
+  console.log('CONNECTION FAILED! no connection to fishHuntStore database @ MongoDB')
+})
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ dbConfiguration/database.js
@@ -0,0 +1,10 @@
+// mongo db connection
+module.exports = {
+    'database': 'mongodb+srv://Nick:consumable@cluster0.krrnb.mongodb.net/fishHuntStore'
+    //github login 
+    // 'github': {
+    //     'clientId': 'ac33d0c52fd417b8e6d3',
+    //     'clientSecret': 'a6b164455ba9860fe21492c6ac30ce033f295bb9',
+    //     'callbackUrl': 'http://localhost:3000/github/callback'
+    // }
+}
