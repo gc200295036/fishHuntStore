f73997df6adffd0e51412ac75167dfcdac8c87fd
gc200295036 <nickbrazda95@hotmail.com>
2021-04-08 00:18:09 -0400

    npm i express-upload installed dependencies

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 //use installed npm file passport-local-mongoose and add reference
 const plm = require('passport-local-mongoose')
 // create userSchema
-const userSchema = new mongoose.Schema({
+var userSchema = new mongoose.Schema({
     username: String,
     password: String,
     // external login 
